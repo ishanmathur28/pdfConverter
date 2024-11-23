bbca753d083fe69e0c294f727b17f9e5ad3b1724
ishanmathur28 <ishanmathur8791@gmail.com>
2024-11-23 16:35:44 +0530

    requirements updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
+        // target: 'https://pdfconverter-c9xt.onrender.com',
         changeOrigin: true, // Ensures the host header is changed to match the target
         secure: false, // Disable SSL verification for localhost
       },
